246128fc7de090a3d2dd86b2b565554396252229
dmitrisi4 <sichkardimitri@gmail.com>
2021-02-04 23:02:23 +0200

    fix

--- dist/main.7c64cf32.js
+++ dist/main.7c64cf32.js
@@ -30282,14 +30282,6 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = void 0;
 
 var burgerMenu = function burgerMenu() {
-  var mediaQuery = window.matchMedia('(max-width: 768px)');
-
-  if (mediaQuery.matches) {
-    var targetDom = document.getElementById('burger-content');
-    var socialLink = document.getElementById('social-link');
-    targetDom.appendChild(socialLink);
-  }
-
   document.getElementById("burger-btn").onclick = function () {
     open();
   };
@@ -30311,48 +30303,81 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var responsiveElementCarrier = function responsiveElementCarrier() {
-  var insertAfter = function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  };
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
-  var makeMagic = function makeMagic(node, targetNode, anchorNode) {
-    if (window.innerWidth < 991 && targetNode.nextSibling !== node) {
-      insertAfter(node, anchorNode);
-      insertAfter(targetNode, node);
-    } else if (window.innerWidth >= 991 && anchorNode.parentNode != null && node.previousSibling !== anchorNode) {
-      insertAfter(anchorNode, node);
-      node.parentNode.removeChild(anchorNode);
-    }
-  };
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 
-  var smAfterNodes = document.querySelectorAll('[data-sm-after]');
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
-  if (smAfterNodes.length > 0) {
-    smAfterNodes.forEach(function (node) {
-      var targetId = node.dataset.smAfter;
-      console.log(node.dataset);
-      var target = document.getElementById(targetId);
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-      if (target == null) {
-        console.warn("Target with id=".concat(targetId, " not found"));
-        return;
-      }
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-      if (target.nextSibling === node) {
-        console.warn("Node is already after the target with id=".concat(targetId));
-        return;
-      }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-      var anchorNode = document.createComment(targetId);
-      makeMagic(node, target, anchorNode);
-      window.addEventListener('resize', function () {
-        return makeMagic(node, target, anchorNode);
-      }, {
-        passive: true
-      });
-    });
+var insertAfter = function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+};
+
+var moveNode = function moveNode(node, targetNode, anchorNode, size) {
+  if (window.innerWidth < size && targetNode.nextSibling !== node) {
+    insertAfter(node, anchorNode);
+    insertAfter(targetNode, node);
+  } else if (window.innerWidth >= size && anchorNode.parentNode != null && node.previousSibling !== anchorNode) {
+    insertAfter(anchorNode, node);
+    node.parentNode.removeChild(anchorNode);
   }
+};
+
+var processElement = function processElement(node, size) {
+  var targetId = node.dataset["".concat(size.name, "After")];
+  console.log(node.dataset);
+  var target = document.getElementById(targetId);
+
+  if (target == null) {
+    console.warn("Target with id=".concat(targetId, " not found"));
+    return;
+  }
+
+  if (target.nextSibling === node) {
+    console.warn("Node is already after the target with id=".concat(targetId));
+    return;
+  }
+
+  var anchorNode = document.createComment(targetId);
+  moveNode(node, target, anchorNode, size.value);
+  window.addEventListener('resize', function () {
+    return moveNode(node, target, anchorNode, size.value);
+  }, {
+    passive: true
+  });
+};
+
+var sizeMap = {
+  // xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200
+};
+
+var responsiveElementCarrier = function responsiveElementCarrier() {
+  Object.entries(sizeMap).forEach(function (_ref) {
+    var _ref2 = _slicedToArray(_ref, 2),
+        name = _ref2[0],
+        value = _ref2[1];
+
+    var smAfterNodes = document.querySelectorAll("[data-".concat(name, "-after]"));
+
+    if (smAfterNodes.length > 0) {
+      smAfterNodes.forEach(function (node) {
+        return processElement(node, {
+          name: name,
+          value: value
+        });
+      });
+    }
+  });
 };
 
 var _default = responsiveElementCarrier;
