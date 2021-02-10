60e0e0b2d0a33cfd1e8349f85206c6eecb5c149f
dmitrisi4 <sichkardimitri@gmail.com>
2021-02-10 13:01:08 +0200

    fix header icons

--- dist/wysiwyg-comments.0bd36445.js
+++ dist/wysiwyg-comments.0bd36445.js
@@ -20867,7 +20867,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62771" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64905" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
