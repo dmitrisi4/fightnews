ca35d0f6e291723010dfa6ee237183194cd83de9
dmitrisi4 <sichkardimitri@gmail.com>
2021-03-18 11:52:26 +0200

    test ytplayer

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -44,4 +44,20 @@ function onPlayerReady(event) {
 	event.target.playVideo();
 }
 
-	loadPlayer();
+	// loadPlayer();
+
+document.querySelectorAll('[data-async-frame]').forEach(player => {
+	let dataSrc = player.dataset.src;
+	let previewSrc = player.dataset.previewSrc;
+	let srcdoc = `
+		<style>*{padding:0;margin:0;overflow:hidden}
+			html,body{height:100%}
+			img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}
+			span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
+		</style>
+		<a href=${dataSrc}>
+			<img src=${previewSrc} alt='Demo video'>
+			<span>▶</span>
+		</a>`
+	player.setAttribute('srcdoc', srcdoc)
+})
