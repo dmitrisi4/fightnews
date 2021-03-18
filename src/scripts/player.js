2702ec2119dfbb97ba64e2e5031398d4a10586a5
dmitrisi4 <sichkardimitri@gmail.com>
2021-03-18 11:07:59 +0200

    test ytplayer

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -18,6 +18,12 @@ function onYouTubePlayer() {
 		// height: '360',
 		// width: '640',
 		videoId: '5qap5aO4i9A',
+		playerVars: {
+			'autoplay': 1,
+			'controls': 0,
+			'wmode': 'opaque',
+			'origin': 'https://dmitrisi4.github.io/'
+		},
 		events: {
 			'onReady': onPlayerReady,
 		}
@@ -32,7 +38,7 @@ function onPlayerReady(event) {
 \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}
 \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    </style>
 \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    <a href=https://www.youtube.com/embed/5qap5aO4i9A?autoplay=1>
-\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    <img loading=lazy src='https://a.espncdn.com/photo/2020/1024/official_mma_power_rankings_v1_1296x729.jpg' alt='Demo video'>
+\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    <img src='https://a.espncdn.com/photo/2020/1024/official_mma_power_rankings_v1_1296x729.jpg' alt='Demo video'>
 \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    <span>▶</span>
 \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t    </a>`);
 	event.target.playVideo();
